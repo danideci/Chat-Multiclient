5148de1c9fa7f41b7d24e73b2313f295b66bf23f
Lorenzo Cremonesi <zempry@gmail.com>
2015-10-26 18:02:30 +0100

    Update serverPort.java
    versione (semi)finale, togliere il commento dal serverPortListener [riga 58] per poter richiamare la classe client con il Socket

--- serverPort.java
+++ serverPort.java
@@ -10,9 +10,11 @@ public class serverPort
 {
 	public static void main(String[] args) 
 	{
-		//dichiarazioni
-		JFrame f = new JFrame("Benvenuto in the MultiChat");
-			f.setSize(300, 180);
+		//declarations
+		
+		JFrame f = new JFrame("Welcome in MultiChat");
+			f.setSize(280, 190);
+			f.setResizable(false);
 			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel p = new JPanel();
 			f.add(p);
@@ -21,33 +23,39 @@ public class serverPort
 			c.add(p);
 		String ip;
 		String port;
-		//grafic server ip address
+		
+		//graphic server ip address
+
+		String serversays = "Please insert Server's IP and Port";
+		JLabel l3 = new JLabel(serversays+"");
+			l3.setBounds(10,10,250,25);
+			p.add(l3);
+
 		JLabel l1 = new JLabel("IP address");
-			l1.setBounds(10, 10, 80, 25);
+			l1.setBounds(10, 50, 80, 25);
 			p.add(l1);
 		
 		JTextField f1 = new JTextField();		
-			f1.setBounds(100, 10, 160, 25);
+			f1.setBounds(100, 50, 160, 25);
 			p.add(f1);
+		
 		//grafic port
+		
 		JLabel l2 = new JLabel("Port");
-			l2.setBounds(10, 40, 80, 25);
+			l2.setBounds(10, 80, 80, 25);
 			p.add(l2);
 			
 		JTextField f2 = new JTextField();
-			f2.setBounds(100, 40, 160, 25);
+			f2.setBounds(100, 80, 160, 25);
 			p.add(f2);
+		
 		//enter button and server message
+		
 		JButton b = new JButton("Enter");
-			b.setBounds(180,80,80,25);
+			b.setBounds(180,120,80,25);
 			p.add(b);
 			
-		String serversays = "Please insert Server's IP and Port";
-		JLabel l3 = new JLabel(serversays+"");
-			l3.setBounds(10,120,250,25);
-			p.add(l3);
-			
-			//b.addActionListener(new LoginListener(f1,f2,l3));
+			//b.addActionListener(new serverPortListener(f1,f2,l3));
 			
 			f.setVisible(true);
 	}
