119fec1dc9d0cee6455100170c72c3a9e1a0c2e7
Riva95 <riva95.dr@gmail.com>
2015-11-30 11:49:36 +0100

    Modifiche
    Modifica file chat e aggiunta GestoneMessaggi

---
+++ client/GestioneMessaggi.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public GestioneMessaggi
+{
+	/*private JTextArea Ta;
+	private String chatArea;
+	private String messaggio=("messaggio");
+	
+	public GestioneMessaggi(JTextArea Ta)*/
+}
--- gui/Chat.java
+++ gui/Chat.java
@@ -5,100 +5,253 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.text.*;
+import java.awt.Color.*;
+import javax.swing.border.*;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
-public class Chat
+public class Chat extends JFrame implements ActionListener
 {
-	public Chat()
+	private static final long serialVersionUID = 1L;
+	private JTextArea taWrite;
+	
+	public Chat() 
 	{
-		JFrame f = new JFrame("MultiChat");
-			//f.setTitle();
-			f.setSize(1024, 600);
-			f.setLocation(100,100);
-			f.setResizable(false);
-			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
-		JPanel p = new JPanel();
-			f.add(p);
-			p.setLayout(null);
-			Container c = f.getContentPane();
-			c.add(p);
+		setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+		Border eBorder = BorderFactory.createEtchedBorder();
 		
-
+		//Create MenuBar
+		
 		JMenuBar mbMenu = new JMenuBar();
-			f.setJMenuBar(mbMenu);
+		setJMenuBar(mbMenu);
 			
-			JMenu file= new JMenu("Options");
-			mbMenu.add(file);
-			//mbMenu.setBounds(10,45,70,25);
-			//p.add(mbMenu);
-				JMenuItem color = new JMenuItem("Choose color");
-				file.add(color);
+		JMenu file= new JMenu("Options");
+		mbMenu.add(file);
+			JMenu color = new JMenu("Choose color");
+			JMenuItem color1 = new JMenuItem("Color1");
+			JMenuItem color2 = new JMenuItem("Color2");
+			file.add(color);
+			color.add(color1);
+			color.add(color2);
 			
-				JMenuItem theme = new JMenuItem("Choose theme");
-				file.add(theme);
+			JMenu theme = new JMenu("Choose Theme");
+			JMenuItem theme1 = new JMenuItem("Theme1");
+			JMenuItem theme2 = new JMenuItem("Theme2");
+			file.add(theme);
+			theme.add(theme1);
+			theme.add(theme2);
 				
-				JMenuItem font = new JMenuItem("Choose font");
-				file.add(font);
+			JMenuItem font = new JMenuItem("Choose font");
+			file.add(font);
 				
-				JMenuItem nickname = new JMenuItem("Change nickname");
-				file.add(nickname);
+			JMenuItem nickname = new JMenuItem("Change nickname");
+			file.add(nickname);
 				
-				JMenuItem delete = new JMenuItem("Delete chat");
-				file.add(delete);
+			JMenuItem delete = new JMenuItem("Delete chat");
+			file.add(delete);
 				
-				file.addSeparator();
+			file.addSeparator();
 				
-				JMenuItem logout = new JMenuItem("Logout");
-				file.add(logout);
-			
-			
-		/*JButton bLogout = new JButton("<html><h4><font color=#800000>Logout</font></h4></html>");
-			bLogout.setBounds(10,10,70,25);
-			p.add(bLogout);
-			
-		JButton bOptions = new JButton("<html><h4>Options</h4></html>");
-			bOptions.setBounds(10,45,70,25);
-			p.add(bOptions);*/
-			
-		JLabel lConnected = new JLabel("<html><b><h3>Connected User</h3></b></html>");
-			lConnected.setBounds(40,10,180,25);
-			//lConnected.setBounds(40,80,180,25);
-			p.add(lConnected);
-			
-		JTextArea tConnectedArea = new JTextArea("");
-			tConnectedArea.setBounds(25,45,150,475);
-			tConnectedArea.setEditable(false);
-			p.add(tConnectedArea);			
-			
-		JLabel lTitle = new JLabel("<html><b><h1>MultiChat</h1></b></html>");
-			lTitle.setBounds(200,10,120,25);
-			//lTitle.setFont(lTitle.getFont().deriveFont(23.0f));
-			p.add(lTitle);
+			JMenuItem logout = new JMenuItem("Logout");
+			file.add(logout);
 		
-		JButton bAllegato = new JButton("<html><b><h3>Attached</h3></b></html>");
-			bAllegato.setBounds(900,10,100,25);
-			p.add(bAllegato);
-			
-		JTextArea tChatArea = new JTextArea("");
-			tChatArea.setBounds(200,46,800,450);
-			tChatArea.setEditable(false);
-			p.add(tChatArea);			
-			
-		JButton bEmot = new JButton("<html><b><h3>Emot</h3></b></html>");
-			bEmot.setBounds(200,506,75,25);
-			p.add(bEmot);
-			
-		JTextField fChat = new JTextField(/*NOT IMPLEMENTED*/"");
-			fChat.setBounds(285,506,625,25);
-			//In case we want to clear the Field when mouse clicks on it
-			//***NOT IMPLEMENTED BECAUSE DOES NOT WORK WITH TAB***
-				/*	public void mouseClicked(MouseEvent e){fChat.setText("");}});	*/
-			p.add(fChat);
-			
-		JButton bEnter = new JButton("<html><b><h3>Enter</h3></b></html>");
-			bEnter.setBounds(925,506,75,25);
-			p.add(bEnter);
-			
-		f.setVisible(true);
+		//Create panelUser
+		
+		JPanel pUser = new JPanel();
+		pUser.setBorder(BorderFactory.createTitledBorder(eBorder));
+        gbc.gridx = 0;
+		gbc.gridy = 0;
+        gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.weightx = 10;
+		gbc.weighty = 100;
+		gbc.insets = new Insets(5,5,5,5);
+		//1_nord
+		//2_ovest
+		//3_sud
+		//4_est
+        add(pUser, gbc);
+		
+		pUser.setLayout(new GridBagLayout());
+		GridBagConstraints gbcUser = new GridBagConstraints();
+		
+		//Create JLabel Connected User
+	
+		JLabel lConnected = new JLabel("<html><font face='AR BERKLEY' size='5'>Connected User</font></html>");
+		lConnected.setBorder(BorderFactory.createTitledBorder(eBorder));
+		gbcUser.gridx = 0;
+		gbcUser.gridy = 0;
+		gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+		gbcUser.fill = GridBagConstraints.BOTH;
+        gbcUser.anchor = GridBagConstraints.NORTHWEST;
+        gbcUser.weightx = 100;
+		gbcUser.weighty = 4;
+		gbcUser.insets = new Insets(5,5,5,5);
+        pUser.add(lConnected, gbcUser);
+		
+		//Create JTextArea Connected User
+		
+		JTextArea taUser = new JTextArea();
+		taUser.setBorder(BorderFactory.createTitledBorder(eBorder));
+        gbcUser.gridx = 0;
+        gbcUser.gridy = 1;
+        gbcUser.weightx = 100;
+		gbcUser.weighty = 96;
+		taUser.setEditable(false);
+		gbcUser.insets = new Insets(5,5,5,5);
+        pUser.add(taUser, gbcUser);
+				
+		//Create panelChat
+		
+		JPanel pChat = new JPanel();
+		pChat.setBorder(BorderFactory.createTitledBorder(eBorder));
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = 90;
+		gbc.weighty = 100;
+		gbc.insets = new Insets(5,5,5,5);
+        add(pChat, gbc);
+		
+		pChat.setLayout(new GridBagLayout());
+		GridBagConstraints gbcChat = new GridBagConstraints();
+		
+		//Create title
+		
+		JLabel lTitle = new JLabel("<html><font face='AR BERKLEY' size='5'>MultiChat</font></html>");
+		lTitle.setBorder(BorderFactory.createTitledBorder(eBorder));
+		gbcChat.gridx = 0;
+		gbcChat.gridy = 0;
+		gbcChat.gridwidth = 2;
+        gbcChat.gridheight = 1;
+		gbcChat.fill = GridBagConstraints.BOTH;
+        gbcChat.anchor = GridBagConstraints.NORTHWEST;
+		gbcChat.weightx = 90;
+		gbcChat.weighty = 4;
+		gbcChat.insets = new Insets(5,5,5,5);
+        pChat.add(lTitle, gbcChat);
+		
+		//Create attached button
+		
+		JButton bAttached = new JButton("Attached");
+		bAttached.setBorder(BorderFactory.createTitledBorder(eBorder));
+		bAttached.setMaximumSize(new Dimension(150,100));
+		gbcChat.gridx = 2;
+		gbcChat.gridy = 0;
+		gbcChat.gridwidth = 1;
+        gbcChat.gridheight = 1;
+        gbcChat.weightx = 10;
+		gbcChat.weighty = 4;
+		gbcChat.insets = new Insets(5,5,5,5);
+        pChat.add(bAttached, gbcChat);
+		
+		//Create chat area
+		
+		JTextArea taChat = new JTextArea("");
+		taChat.setBorder(BorderFactory.createTitledBorder(eBorder));
+		gbcChat.gridx = 0;
+		gbcChat.gridy = 1;
+		gbcChat.gridwidth = 3;
+        gbcChat.gridheight = 1;
+        gbcChat.weightx = 100;
+		gbcChat.weighty = 89;
+		gbcChat.insets = new Insets(5,5,5,5);
+		taChat.setEditable(false);
+        pChat.add(taChat, gbcChat);
+		
+		//Create emot button
+		
+		JButton bEmot = new JButton("Emot");
+		bEmot.setBorder(BorderFactory.createTitledBorder(eBorder));
+		//bEmot.setPreferredSize(new Dimension(pChat.getWidth()/6, 15));
+		//bEmot.setMinimumSize(new Dimension(pChat.getWidth()/6, 15));
+		bEmot.setMaximumSize(new Dimension(150,100));
+		gbcChat.gridx = 0;
+		gbcChat.gridy = 2;
+		gbcChat.gridwidth = 1;
+        gbcChat.gridheight = 1;
+        gbcChat.weightx = 10;
+		gbcChat.weighty = 7;
+		gbcChat.insets = new Insets(5,5,5,5);
+		pChat.add(bEmot, gbcChat);
+		
+		//Create field of writing 
+		
+		taWrite = new JTextArea("");
+		JScrollPane spWrite = new JScrollPane(taWrite);
+		spWrite.setPreferredSize(new Dimension(pChat.getWidth()*(4/6), 15));
+		spWrite.setMinimumSize(new Dimension(pChat.getWidth()*(4/6), 15));
+		taWrite.setLineWrap(true);
+		//taWrite.setLineWrapWord(true);
+		spWrite.setBorder(BorderFactory.createTitledBorder(eBorder));
+		gbcChat.gridx = 1;
+		gbcChat.gridy = 2;
+		gbcChat.gridwidth = 1;
+        gbcChat.gridheight = 1;
+        gbcChat.weightx = 80;
+		gbcChat.weighty = 7;
+		gbcChat.insets = new Insets(5,5,5,5);
+        pChat.add(spWrite, gbcChat);
+		
+		//Create enter button
+		
+		JButton bEnter = new JButton("Enter");
+		//bEnter.setPreferredSize(new Dimension(pChat.getWidth()/6, 15));
+		//bEnter.setMinimumSize(new Dimension(pChat.getWidth()/6, 15));
+		bEnter.setBorder(BorderFactory.createTitledBorder(eBorder));
+		bEnter.setMaximumSize(new Dimension(150,100));
+		gbcChat.gridx = 2;
+		gbcChat.gridy = 2;
+		gbcChat.gridwidth = 1;
+        gbcChat.gridheight = 1;
+        gbcChat.weightx = 10;
+		gbcChat.weighty = 7;
+		gbcChat.insets = new Insets(5,5,5,5);
+        pChat.add(bEnter, gbcChat);
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(1280, 720));
+		setMaximumSize(new Dimension(1920, 1080));
+		setMinimumSize(new Dimension(640, 480));
+		setVisible(true);
+        pack();
 	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		String pulsante = e.getActionCommand();
+		
+		if(pulsante.equals("Enter"))
+		{
+			if(!(taWrite.getText().equals("")))
+			{
+				//GestioneMessaggi mess = new GestioneMessagi(taWrite);
+			}
+			else
+			{
+				
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+        javax.swing.SwingUtilities.invokeLater(new Runnable() 
+		{ 
+			@Override
+            public void run() 
+			{
+                Chat chat = new Chat();
+            }
+        });
+    }
 }
